4c462772ce3c19f89d272debc59df3fb39516af1
meisibley <mkong99@yahoo.com>
2023-08-11 20:41:47 -0700

    whatever

--- exe_funcs.c
+++ exe_funcs.c
@@ -23,7 +23,7 @@ void tokenize(char *line_buf, unsigned int count, stack_t **stack)
 		if (i == 1)
 			value = strdup(token);
 		if (i == 2)
-			fprintf(stderr, "\n");
+			fprintf(stderr, "");
 		i++;
 		token = strtok(NULL, delim);
 	}
